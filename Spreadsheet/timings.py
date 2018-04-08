1a2952bdaff93faf59895ef448b76a0c76116398
spencrr <23708360+spencrr@users.noreply.github.com>
2018-04-08 12:50:51 -0500

    much faster batch cell updates in labels.py

--- Spreadsheet/timings.py
+++ Spreadsheet/timings.py
@@ -1,7 +1,6 @@
 import time
 import average_each_team, improved, labels, getsheet
 
-
 q = 'Qualification Responses'
 
 getsheet.open_spreadsheet(q)
@@ -9,9 +8,9 @@ getsheet.open_spreadsheet(q)
 timer = time.time()
 average_each_team.main(q)
 print(time.time() - timer)
-timer = time.time()
-improved.main(q)
-print(time.time() - timer)
+# timer = time.time()
+# improved.main(q)
+# print(time.time() - timer)
 timer = time.time()
 labels.main(q)
 print(time.time() - timer)
