8a3f84ff10ce56250ec472b9d4e47971d9fe0bab
spencrr <23708360+spencrr@users.noreply.github.com>
2018-04-14 15:10:59 -0500

    done

--- SimpleNN/network.py
+++ SimpleNN/network.py
@@ -1,5 +1,6 @@
-class network:
     import numpy as np
+
+class network:
 
     @staticmethod
     def relu(z, d=False):
@@ -15,27 +16,25 @@ class network:
         return 0.5 * network.np.sum((a - b) ** 2)
 
     def __init__(self, input, v, output):
-        # np.random.seed(1)
         v.append(output)
         self.syn = []
         for l in range(len(v)):
-            self.syn.append(network.np.ndarray(shape=(v[l], input), buffer=np.random.uniform(-1, 1, v[l] * (input + 1))))
+            self.syn.append(np.ndarray(shape=(v[l], input), buffer=np.random.uniform(-1, 1, v[l] * (input + 1))))
             input = v[l]
         self.syn = np.array(self.syn)
+        print(self.syn)
 
     def feed(self, input_vector):
         self.output_matrix = []
         for l in self.syn:
-            print(input_vector, l.T)
             input_vector = network.relu(np.dot(input_vector, l.T))
             self.output_matrix.append(input_vector)
         return input_vector
 
 
 def main():
-    n = network(100, [100, 100, 100, 100, 100], 100)
-    import numpy as np
-    n.feed(np.random.uniform(-1, 1, 100))
+    n = network(2, [], 2)
+    n.feed(np.random.uniform(-1, 1, 2))
 
 if __name__ == '__main__':
     main()
