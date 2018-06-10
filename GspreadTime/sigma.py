cd597671b34512f40f0e7986fad5279cf449cc0e
spencrr <23708360+spencrr@users.noreply.github.com>
2018-06-10 15:13:39 -0500

    added open spreadsheet to sigma

--- GspreadTime/sigma.py
+++ GspreadTime/sigma.py
@@ -1,2 +1,16 @@
 import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
+def open_spreadsheet(client_open_key):
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+    return client.open(client_open_key)
+
+
+def main():
+	speadsheet = open_spreadsheet()
+
+if __name__ == '__main__':
+	main()
+	exit()
