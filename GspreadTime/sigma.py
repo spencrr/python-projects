47fc943e7571ebcc3242ad834888489cc0eb6235
spencrr <23708360+spencrr@users.noreply.github.com>
2018-06-10 20:07:17 -0500

    added print functionality

--- GspreadTime/sigma.py
+++ GspreadTime/sigma.py
@@ -1,15 +1,19 @@
 import gspread
+from pprint import pprint
 from oauth2client.service_account import ServiceAccountCredentials
 
 def open_spreadsheet(client_open_key):
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
     client = gspread.authorize(creds)
-    return client.open(client_open_key)
+    return client.open_by_key(client_open_key)
 
 
 def main():
-	speadsheet = open_spreadsheet()
+	spreadsheet = open_spreadsheet('1vptL2WMDbj6XNyTgOICG3KBoa98yoVHr1W8pCVl8SUY')
+	sheet = spreadsheet.get_worksheet(0)
+	pprint(sheet.get_all_values())
+
 
 if __name__ == '__main__':
 	main()
